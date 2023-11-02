6d44fac04652502be43e707d5348b2cd3863b931
Stefano Frasson Pianizzola <frasson.stefano@gmail.com>
2023-11-02 01:31:48 +0100

    First commit

---
+++ navigation-mega-menu.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Navigation Mega Menu
+ * Description:       This is a gutenberg block to have a mega menu on navigation. 
+ * Requires at least: 6.2
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Stefano Frasson Pianizzola
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       mega-menu
+ * Domain Path:       /languages
